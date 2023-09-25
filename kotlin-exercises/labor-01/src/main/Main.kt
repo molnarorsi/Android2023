e8e79ef44c9d6eebe1cb52bd554e5a9a68860c20
molnarorsi <79136111+molnarorsi@users.noreply.github.com>
2023-09-25 23:30:23 +0300

    init

--- kotlin-exercises/labor-01/src/main/Main.kt
+++ kotlin-exercises/labor-01/src/main/Main.kt
@@ -2,7 +2,7 @@ package main
 
 fun main(args: Array<String>) {
     println("Hello World!")
-
+    println("Szia!")
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
