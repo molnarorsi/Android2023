75196b3b4173060c9df74a819e4395ca12f8e7cb
molnarorsi <79136111+molnarorsi@users.noreply.github.com>
2023-09-26 16:05:03 +0300

    first exercise

--- kotlin-exercises/labor-01/src/main/Main.kt
+++ kotlin-exercises/labor-01/src/main/Main.kt
@@ -1,8 +1,10 @@
 package main
 
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
    while(true) {
-       print("Enter the number of the exercise:")
+       print("Enter the number of the exercise: \nEnter 0 to exit!")
        val exercise = readLine()
        if(exercise == null || exercise == "") {
            println("Please enter a valid exercise number!")
@@ -10,12 +12,17 @@ fun main(args: Array<String>) {
        }
 
        val number = try {
-           input.toInt()
+           exercise.toInt()
        } catch (err: NumberFormatException) {
-           println("'$input' is not a number!")
-           continuue
+           println("'$exercise' is not a number!")
+           continue
        }
 
+       when(number) {
+           0 -> {
+               exitProcess(0)
+           }
+           1 -> stringTemplate()
        }
    }
 }
@@ -27,3 +34,5 @@ fun stringTemplate() {
     val nr2 = 3
     println("$nr1 + $nr2 = ${nr1 + nr2}")
 }
+
+//-------------EXERCISE 2.-------------//
