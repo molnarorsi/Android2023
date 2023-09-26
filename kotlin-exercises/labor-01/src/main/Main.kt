b1565070e6efc3b0221df26efa03d242161220a3
molnarorsi <79136111+molnarorsi@users.noreply.github.com>
2023-09-26 19:29:06 +0300

    exercise 3-4-5

--- kotlin-exercises/labor-01/src/main/Main.kt
+++ kotlin-exercises/labor-01/src/main/Main.kt
@@ -1,11 +1,12 @@
 package main
 
+import java.util.Base64
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
    while(true) {
        print("Enter the number of the exercise: \nEnter 0 to exit!")
-       val exercise = readLine()
+       val exercise = readlnOrNull()
        if(exercise == null || exercise == "") {
            println("Please enter a valid exercise number!")
            continue
@@ -24,6 +25,11 @@ fun main(args: Array<String>) {
            }
            1 -> stringTemplate()
            2 -> daysList()
+           3 -> primeNumbersInGivenRange(IntRange(1, 20))
+           4 -> encodeAndDecodeMessage()
+           5 -> {
+               printEvenNumbers((1..10).toList())
+           }
        }
    }
 }
@@ -68,3 +74,47 @@ fun dayOfLength(list: List<String>, length: Int) {
     println("\nDays with given length $length:")
     list.filter { it.length == length }.forEach { println(it) }
 }
+
+//-------------EXERCISE 3.-------------//
+fun isPrimeNumber(number: Int): Boolean {
+    for (i in 2..number / 2) {
+        if (number % i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+fun primeNumbersInGivenRange(range: IntRange) {
+    println("Prime numbers in the $range range:\n")
+    range.filter { isPrimeNumber(it) }.forEach{print("$it, ")}
+    print("\n")
+}
+
+//-------------EXERCISE 4.-------------//
+fun encodeAndDecodeMessage() {
+    val originalMessage = "Hello from the other side! :)"
+
+    val encodedMessage = messageCoding(originalMessage, ::encodeMessage)
+    val decodedMessage = messageCoding(encodedMessage, ::decodeMessage)
+
+    println("Original message: $originalMessage")
+    println("Encoded message: $encodedMessage")
+    println("Decoded message: $decodedMessage")
+}
+fun encodeMessage(msg: String): String {
+    val encodedBytes = Base64.getEncoder().encode(msg.toByteArray())
+    return String(encodedBytes)
+}
+
+fun decodeMessage(encodedMsg: String): String {
+    val decodedBytes = Base64.getDecoder().decode(encodedMsg.toByteArray())
+    return String(decodedBytes)
+}
+
+fun messageCoding(msg: String, func: (String) -> String): String {
+    return func(msg)
+}
+
+//-------------EXERCISE 5.-------------//
+fun printEvenNumbers(numbers: List<Int>) = numbers.filter { it % 2 == 0 }.forEach { print(it) }
