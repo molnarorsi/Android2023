311d50ba2b5006609267c9338623ac5a291572d7
molnarorsi <molnar.orsolya@student.ms.sapientia.ro>
2023-11-06 15:09:21 +0200

    started working on lab5

---
+++ android-project/RecipesHub/app/src/main/java/com/example/recipeshub/activities/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.recipeshub.activities
+
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.recipeshub.databinding.ActivityMainBinding
+import com.example.recipeshub.databinding.ActivitySplashBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val message = intent.getStringExtra("message")
+        binding.textView.text = message
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: MainActivity started.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: MainActivity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: MainActivity paused.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: MainActivity stopped.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: MainActivity destroyed.")
+    }
+
+}
---
+++ android-project/RecipesHub/app/src/main/java/com/example/recipeshub/activities/SplashActivity.kt
@@ -0,0 +1,51 @@
+package com.example.recipeshub.activities
+
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.recipeshub.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener {
+            val message = binding.editText.text.toString()
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("message", message)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: SplashActivity started.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: SplashActivity resumed.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: SplashActivity paused.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: SplashActivity stopped.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: SplashActivity destroyed.")
+    }
+
+}
