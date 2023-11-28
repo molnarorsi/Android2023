82b9ab6d840caf54685451ef00055150cbeaaf6b
molnarorsi <molnar.orsolya@student.ms.sapientia.ro>
2023-11-28 18:03:29 +0200

    designed recipes list

--- android-project/RecipesHub/app/src/main/java/fragments/recipe/adapter/RecipesListAdapter.kt
+++ android-project/RecipesHub/app/src/main/java/fragments/recipe/adapter/RecipesListAdapter.kt
@@ -13,33 +13,37 @@ import com.example.recipeshub.R
 import com.example.recipeshub.databinding.RecipeListItemBinding
 import repository.recipe.model.RecipeModel
 
-class RecipesListAdapter(private var recipesList: List<RecipeModel>, private val context: Context) :
-    RecyclerView.Adapter<RecipesListAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class RecipesListAdapter(
+    private var recipesList: List<RecipeModel>,
+    private val context: Context) :
+    RecyclerView.Adapter<RecipesListAdapter.RecipeViewHolder>() {
+    class RecipeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val recipeName: TextView
         val recipeDescription: TextView
         val recipePicture: ImageView
+        val ratingsView: TextView
 
         init {
             // Define click listener for the ViewHolder's View
-            recipeName = view.findViewById(R.id.recipeItemTitleView)
-            recipeDescription = view.findViewById(R.id.recipeItemDescriptionView)
-            recipePicture = view.findViewById(R.id.recipeItemPictureView)
+            recipeName = view.findViewById(R.id.recipeTitle)
+            recipeDescription = view.findViewById(R.id.recipeDescription)
+            recipePicture = view.findViewById(R.id.recipePhoto)
+            ratingsView = view.findViewById(R.id.recipeScore)
 
         }
     }
 
     // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecipeViewHolder {
         // Create a new view, which defines the UI of the list item
         val view = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.recipe_list_item, viewGroup, false)
 
-        return ViewHolder(view)
+        return RecipeViewHolder(view)
     }
 
     // Replace the contents of a view (invoked by the layout manager)
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: RecipeViewHolder, position: Int) {
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
